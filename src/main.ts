576dde4d7cf2d5aa41ee7f137482fe5cbeb34e76
Damián Pumar <damianpumar@gmail.com>
2025-06-14 22:07:52 +0200

    feat: update ReactComponent to return a string representation of a div

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,16 @@ const greet = (name: string): string => {
 };
 
 console.log(greet("TypeScript Developer"));
+
+
+const ReactComponent = () => {
+  return (
+    // <div>
+    //   <h1>Welcome to TypeScript!</h1>
+    //   <p>This is a simple component.</p>
+    // </div>
+    "<div>"
+  );
+}
+
+console.log(ReactComponent());
